c25bbd876f579d74165c0e0c03068e721ffc2b64
Danil Bikmetov <danilbikmetov95gmail.com>
2022-10-11 17:05:55 +0300

    Homework1

---
+++ Homework1/Homework1.py
@@ -0,0 +1,11 @@
+# С помощью использования **лямбд, filter, map, zip, enumerate, list comprehension
+#1# lambda #
+
+dooble = lambda x: x*2
+print(dooble(5))
+
+#2#
+#def dooble(x):
+#    return x * 2
+#print(dooble(5))
+
