f4591b850b9f5cade191293e1d16f6c52c79d5ac
Danil Bikmetov <danilbikmetov95gmail.com>
2022-10-11 18:38:55 +0300

    Homework4

---
+++ Homework4/Homework4.py
@@ -0,0 +1,13 @@
+#1# enumerate
+ 
+#lst = [1, 3, 4, 6, 10, 11, 15, 12, 14]
+#for num,val in enumerate(lst):
+#    print(num ,"=" ,val)
+
+#2#
+lst = [1, 3, 4, 6, 10, 11, 15, 12, 14]
+ind=0
+for number in lst:
+    ind+=1
+    print(ind ," = ", number )
+    
