4f86bd7fdc6be24974e50829c285656b187ee462
Danil Bikmetov <danilbikmetov95gmail.com>
2022-10-11 18:20:57 +0300

    Homework3

---
+++ Homework3/Homework.py
@@ -0,0 +1,10 @@
+#1# map
+#lst = [1, 3, 4, 6, 10, 11, 15, 12, 14]
+#new_list = list(map(lambda x: x*2,lst))
+#print(new_list)
+
+#2#
+#lst = [1, 3, 4, 6, 10, 11, 15, 12, 14]
+#result = [num * 2 for num in lst]
+#print(result)
+
